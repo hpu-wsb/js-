e2fe1b796643148ff63efdc45e87b508af0e797d
寒月 <101851966+hpu-wsb@users.noreply.github.com>
2022-08-18 14:52:58 +0800

    Create 选择排序.js

---
+++ js学习笔记/选择排序.js
@@ -0,0 +1,12 @@
+function sort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] > arr[j]) {
+                [arr[i], arr[j]] = [arr[j], arr[i]]
+            }
+        }
+    }
+    return arr
+}
+let arr = [6, 4, 9, 8, 1, 3, 2]
+console.log(sort(arr))
